caa6fcf16630dad6da52132826ef6038e8f7c281
Sangam Arora <arorasam2109@gmail.com>
2023-07-04 20:16:59 +0530

    script updated

--- script.js
+++ script.js
@@ -2,7 +2,11 @@
 function setButtonId(buttonId) {
     localStorage.setItem('buttonId', buttonId);
   }
-
+  
+  // Function to get the button identifier
+//   function getButtonId() {
+//     return localStorage.getItem('buttonId');
+//   }
 let activeButton = null;
 
 function toggleButton(button) {
@@ -18,20 +22,24 @@ function toggleButton(button) {
   }
 }
 
-  function handlesubmit() {
-    let submitButton = document.getElementById("submit");
-    submitButton.disabled = true;
-  
-    let ratingButtons = document.querySelectorAll(".box");
-    for (let i = 0; i < ratingButtons.length; i++) {
-      let button = ratingButtons[i];
-      if (button.classList.contains("active")) {
-        submitButton.disabled = false;
-        window.location.href = 'thankyou.html';
-        break;
-      }
+function handlesubmit() {
+  let submitButton = document.getElementById("submit");
+  submitButton.disabled = true;
+
+  let ratingButtons = document.querySelectorAll(".box");
+  for (let i = 0; i < ratingButtons.length; i++) {
+    let button = ratingButtons[i];
+    if (button.classList.contains("active")) {
+      submitButton.disabled = false;
+      window.location.href='thankyou.html';
+      return; // Exit the function and prevent navigation
     }
   }
+
+  // If no rating button is active, show an alert message
+  window.alert("Please select a rating button");
+}
+
   
   
     
